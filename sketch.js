6853aa849014f19c18aba9b3321bebd9ebbe9966
Rithikaravind <88716005+Rithikaravind@users.noreply.github.com>
2021-08-16 11:43:32 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var marks = [35,38,42,45,43,34,46,41,48,32];
+
+
+function display(){
+  for(var i = 0; i<marks.length; i++){
+    if(marks[i]>45){
+      console.log(marks[i])
+    }
+   
+  }
+}
+
+function setup() 
+{
+  createCanvas(400,400);
+  display();
+}
+
+function draw() 
+{
+  background(30);
+}
+
+ 
+
